11f3aecf3d011e65789bf81207adf3a5f16d43b5
IlluzionzDev <jaminstratford@gmail.com>
2020-05-01 11:38:56 +1200

    Refactor NMS

---
+++ 1_15_R1/src/main/java/com/illuzionzstudios/tab/NMS_1_15_R1.java
@@ -0,0 +1,17 @@
+package com.illuzionzstudios.tab;
+
+import org.bukkit.craftbukkit.v1_15_R1.entity.CraftPlayer;
+import org.bukkit.entity.Player;
+
+public class NMS_1_15_R1 implements NMSHandler {
+
+    @Override
+    public void setAvatar(int x, int y, Player player, Player... players) {
+        this.setAvatar(x, y, ((CraftPlayer) player).getProfile(), players);
+    }
+
+    @Override
+    public void addSkin(Player player, Player... players) {
+        this.addSkin(player.getUniqueId(), ((CraftPlayer) player).getProfile(), players);
+    }
+}
